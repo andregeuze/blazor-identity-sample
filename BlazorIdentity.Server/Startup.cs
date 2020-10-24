2afc2ec365727dd143c5ae6a40db0c1fe1f8c5d3
André Geuze <andregeuze@gmail.com>
2020-10-24 22:52:55 +0200

    Configure the Startup class

--- BlazorIdentity.Server/Startup.cs
+++ BlazorIdentity.Server/Startup.cs
@@ -1,15 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BlazorIdentity.Server.Data;
+using BlazorIdentity.Server.Models;
+using BlazorIdentity.Server.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorIdentity.Server.Data;
+using static OpenIddict.Abstractions.OpenIddictConstants;
 
 namespace BlazorIdentity.Server
 {
@@ -26,8 +26,90 @@ namespace BlazorIdentity.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                // Configure the context to use SQLite.
+                options.UseSqlite(Configuration.GetConnectionString("AppDbContextConnection"));
+
+                // Register the entity sets needed by OpenIddict.
+                // Note: use the generic overload if you need
+                // to replace the default OpenIddict entities.
+                options.UseOpenIddict();
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                // Add more password requirements ...
+            })
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Configure Identity to use the same JWT claims as OpenIddict instead
+            // of the legacy WS-Federation claims it uses by default (ClaimTypes),
+            // which saves you from doing the mapping in your authorization controller.
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.ClaimsIdentity.UserNameClaimType = Claims.Name;
+                options.ClaimsIdentity.UserIdClaimType = Claims.Subject;
+                options.ClaimsIdentity.RoleClaimType = Claims.Role;
+            });
+
+            services.AddOpenIddict()
+
+                // Register the OpenIddict core components.
+                .AddCore(options =>
+                {
+                    // Configure OpenIddict to use the Entity Framework Core stores and models.
+                    // Note: call ReplaceDefaultEntities() to replace the default OpenIddict entities.
+                    options.UseEntityFrameworkCore()
+                           .UseDbContext<AppDbContext>();
+                })
+
+                // Register the OpenIddict server components.
+                .AddServer(options =>
+                {
+                    // Enable the authorization, logout, token and userinfo endpoints.
+                    options.SetAuthorizationEndpointUris("/connect/authorize")
+                           .SetLogoutEndpointUris("/connect/logout")
+                           .SetTokenEndpointUris("/connect/token")
+                           .SetUserinfoEndpointUris("/connect/userinfo");
+
+                    // Mark the "email", "profile" and "roles" scopes as supported scopes.
+                    options.RegisterScopes(Scopes.Email, Scopes.Profile, Scopes.Roles);
+
+                    // Note: the sample uses the code and refresh token flows but you can enable
+                    // the other flows if you need to support implicit, password or client credentials.
+                    options.AllowAuthorizationCodeFlow()
+                           .AllowRefreshTokenFlow();
+
+                    // Register the signing and encryption credentials.
+                    options.AddDevelopmentEncryptionCertificate()
+                           .AddDevelopmentSigningCertificate();
+
+                    // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+                    options.UseAspNetCore()
+                           .EnableAuthorizationEndpointPassthrough()
+                           .EnableLogoutEndpointPassthrough()
+                           .EnableStatusCodePagesIntegration()
+                           .EnableTokenEndpointPassthrough();
+                })
+
+                // Register the OpenIddict validation components.
+                .AddValidation(options =>
+                {
+                    // Import the configuration from the local OpenIddict server instance.
+                    options.UseLocalServer();
+
+                    // Register the ASP.NET Core host.
+                    options.UseAspNetCore();
+                });
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
+
+            services.AddSingleton<IEmailSender, EmailSender>();
             services.AddSingleton<WeatherForecastService>();
         }
 
@@ -37,6 +119,8 @@ namespace BlazorIdentity.Server
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                //app.UseWebAssemblyDebugging(); // Enable for Blazor WASM
             }
             else
             {
@@ -46,14 +130,23 @@ namespace BlazorIdentity.Server
             }
 
             app.UseHttpsRedirection();
+            //app.UseBlazorFrameworkFiles(); // Enable for Blazor WASM
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+
+                endpoints.MapBlazorHub(); // Enable For Blazor Server only
+                endpoints.MapFallbackToPage("/_Host"); // Enable For Blazor Server only
+
+                //endpoints.MapFallbackToFile("index.html"); // Enable for Blazor WASM
             });
         }
     }
--- README.md
+++ README.md
@@ -91,6 +91,108 @@ public class EmailSender : IEmailSender
 }
 ```
 
+Add to Startup.cs, ConfigureServices method:
+
+```csharp
+services.AddDbContext<AppDbContext>(options =>
+{
+    // Configure the context to use SQLite.
+    options.UseSqlite(Configuration.GetConnectionString("AppDbContextConnection"));
+
+    // Register the entity sets needed by OpenIddict.
+    // Note: use the generic overload if you need
+    // to replace the default OpenIddict entities.
+    options.UseOpenIddict();
+});
+
+services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    // Add more password requirements ...
+})
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+// Configure Identity to use the same JWT claims as OpenIddict instead
+// of the legacy WS-Federation claims it uses by default (ClaimTypes),
+// which saves you from doing the mapping in your authorization controller.
+services.Configure<IdentityOptions>(options =>
+{
+    options.ClaimsIdentity.UserNameClaimType = Claims.Name;
+    options.ClaimsIdentity.UserIdClaimType = Claims.Subject;
+    options.ClaimsIdentity.RoleClaimType = Claims.Role;
+});
+
+services.AddOpenIddict()
+    // Register the OpenIddict core components.
+    .AddCore(options =>
+    {
+        // Configure OpenIddict to use the Entity Framework Core stores and models.
+        // Note: call ReplaceDefaultEntities() to replace the default OpenIddict entities.
+        options.UseEntityFrameworkCore()
+                .UseDbContext<AppDbContext>();
+    })
+
+    // Register the OpenIddict server components.
+    .AddServer(options =>
+    {
+        // Enable the authorization, logout, token and userinfo endpoints.
+        options.SetAuthorizationEndpointUris("/connect/authorize")
+                .SetLogoutEndpointUris("/connect/logout")
+                .SetTokenEndpointUris("/connect/token")
+                .SetUserinfoEndpointUris("/connect/userinfo");
+
+        // Mark the "email", "profile" and "roles" scopes as supported scopes.
+        options.RegisterScopes(Scopes.Email, Scopes.Profile, Scopes.Roles);
+
+        // Note: the sample uses the code and refresh token flows but you can enable
+        // the other flows if you need to support implicit, password or client credentials.
+        options.AllowAuthorizationCodeFlow()
+                .AllowRefreshTokenFlow();
+
+        // Register the signing and encryption credentials.
+        options.AddDevelopmentEncryptionCertificate()
+                .AddDevelopmentSigningCertificate();
+
+        // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+        options.UseAspNetCore()
+                .EnableAuthorizationEndpointPassthrough()
+                .EnableLogoutEndpointPassthrough()
+                .EnableStatusCodePagesIntegration()
+                .EnableTokenEndpointPassthrough();
+    })
+
+    // Register the OpenIddict validation components.
+    .AddValidation(options =>
+    {
+        // Import the configuration from the local OpenIddict server instance.
+        options.UseLocalServer();
+
+        // Register the ASP.NET Core host.
+        options.UseAspNetCore();
+    });
+
+// Add our custom Email Sender
+services.AddSingleton<IEmailSender, EmailSender>();
+```
+
+Add to Startup.cs, Configure method:
+
+```csharp
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapControllers();
+
+    endpoints.MapBlazorHub(); // Enable For Blazor Server only
+    endpoints.MapFallbackToPage("/_Host"); // Enable For Blazor Server only
+});
+```
+
 ### Scaffold all the Identity files
 
 ```powershell
